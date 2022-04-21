b3ac2fb89661db4742152dbfc17ea169700057f9
Michael <dizel882@gmail.com>
2022-04-21 18:22:21 +0300

    refactoring

--- src/main/java/HttpLocalClient.java
+++ src/main/java/HttpLocalClient.java
@@ -3,9 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
-public class mClient {
+public class HttpLocalClient {
 
     public static void main(String... args) {
 
